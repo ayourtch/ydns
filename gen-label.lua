ddee365328cfdf833d08229e70b5223eab6ad2e2
Andrew Yourtchenko <ayourtch@gmail.com>
2011-01-03 04:41:16 +0100

    x x x x => x{4} thanks Zed for reminding :-)

--- gen-label.lua
+++ gen-label.lua
@@ -9,22 +9,23 @@ label = ]])
 acc = {}
 for i = 1,63 do
   local delim = "|"
+  local innards = ''
   if (i == 63) then
     delim = ";"
   end 
   we_are_rfc_police = true
   if we_are_rfc_police then
     if i == 0 then 
-      table.insert(acc, "ll")
+      innards = "ll"
     elseif i == 1 then
-      table.insert(acc, "ld")
+      innards = "ll ld"
     else
-      table.insert(acc, 2, "ldh")
+      innards = "ll ldh{" .. i .. "} ld"
     end
   else
-    table.insert(acc, "ldh")
+    innards = "ldh{" .. i .. "}"
   end
-  print("    " .. i .. " " .. table.concat(acc, " ") .. 
+  print("    " .. i .. " " .. innards .. 
         " @{ debug(DNS_LABEL,\"LBL"..i.."\\n\"); seglen = " .. i .. "; } " .. delim)
 end
 
